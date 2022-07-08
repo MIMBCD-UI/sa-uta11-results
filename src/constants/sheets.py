661fb7a92a0c5cbb0b20097a8a1c08f72a2ba476
Francisco Maria Calisto <francisco_mcalisto@outlook.com>
2022-07-08 19:39:35 +0100

    [UPDATE] Sheets Source

--- src/constants/sheets.py
+++ src/constants/sheets.py
@@ -1,10 +1,64 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+"""
+sheets.py: to read our data, we are using several datasets. For that,
+           we need to read a bunch of xls and csv files. The presented file,
+           will serve this purpose. It is here, where we are reading
+           our datasets, provinding several variables to be used.
+"""
+
+import os
+import sys
+
+from os import path
+
 from cmath import nan
 import pandas as pd
 import numpy as np
 import datetime as dt
 
+# The current folder path.
+basePath = os.path.dirname(__file__)
 
-loadData = pd.read_excel ("../../data/mimbcdui_uta11_scenarios.xlsx", sheet_name="results_before", header=None)
+pathReposDirname = os.path.dirname(__file__)
+joinReposPath = os.path.join(pathReposDirname, '..', '..')
+pathReposAbsPath = os.path.abspath(joinReposPath)
+
+src_data_dir = os.path.join(pathReposAbsPath, 'data')
+sys.path.append(src_data_dir)
+srcDataAbsPath = os.path.abspath(src_data_dir)
+
+# ============================== #
+# ============================== #
+
+import pandas as pd
+
+# ============================== #
+# ============================== #
+# ============================== #
+# ============================== #
+
+# ============================== #
+# ============================== #
+#               PATH             #
+# ============================== #
+# ============================== #
+
+scenarios_dir = os.path.join(srcDataAbsPath, 'mimbcdui_uta11_scenarios.xlsx')
+sys.path.append(scenarios_dir)
+scenarios_abs_path = os.path.abspath(scenarios_dir)
+
+times_dir = os.path.join(srcDataAbsPath, 'mimbcdui_uta7_uta11_results_curated_abimid_times.csv')
+sys.path.append(times_dir)
+times_abs_path = os.path.abspath(times_dir)
+
+# ============================== #
+# ============================== #
+# ============================== #
+# ============================== #
+
+loadData = pd.read_excel (scenarios_abs_path, sheet_name="results_before", header=None)
 
 data = np.array(loadData)
 
@@ -17,7 +71,7 @@ for i,x in enumerate(data):
         if isinstance(y, dt.time):
             data[i][j] = y.hour * 60 + y.minute
 
-loadScenarios = pd.read_excel ("../../data/mimbcdui_uta11_scenarios.xlsx", sheet_name="scenarios", header=None)
+loadScenarios = pd.read_excel (scenarios_abs_path, sheet_name="scenarios", header=None)
 scenarios = np.array(loadScenarios)
 
 def getScenariosPatients():
---
+++ src/techniques/evaluations.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+"""
+evaluations.py: evaluate the models using both plots and
+                performance metrics.
+"""
+
+__author__      = "Francisco Maria Calisto"
+__maintainer__  = "Francisco Maria Calisto"
+__email__       = "francisco.calisto@tecnico.ulisboa.pt"
+__license__     = "MIT"
+__version__     = "1.0.0"
+__status__      = "Development"
+__copyright__   = "Copyright 2022, Instituto Superior Técnico (IST)"
+__credits__     = [
+  "João Fernandes",
+  "Bruno Oliveira",
+  "Carlos Santiago",
+  "Jacinto C. Nascimento",
+  "Pedro Miraldo",
+  "Nuno Nunes"
+]
+
+import os
+import sys
+import logging
+
+from os import path
+
+# The current folder path.
+basePath = os.path.dirname(__file__)
+
+# The path to the repository "src" folder.
+joinRepoSrcPath = os.path.join(basePath, '..')
+pathRepoSrcAbsPath = os.path.abspath(joinRepoSrcPath)
+# Add the directory containing the module to
+# the Python path (wants absolute paths).
+sys.path.append(pathRepoSrcAbsPath)
+
+# Appending methods path
+methodsPath = os.path.join(joinRepoSrcPath, 'methods')
+methodsAbsPath = os.path.abspath(methodsPath)
+sys.path.append(methodsAbsPath)
+sys.path.insert(0, methodsAbsPath)
+
+# Importing available methods
+from classifiers import *
+
+def evalModelSpecific(df_real, df_pred):
+	ccm = classConfusionMatrix(df_real, df_pred)
+	cr = classReport(df_real, df_pred)
+	return cr, ccm
+
+# ==================== END File ==================== #
